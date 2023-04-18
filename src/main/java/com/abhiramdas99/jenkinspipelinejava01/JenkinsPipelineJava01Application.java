a95f0d5fa4fcdb964a6373ddfc765bcd7483dc4a
abhiramdas99 <abhiramdas99@gmail.com>
2023-04-18 18:33:12 +0530

    this my 1st commit

---
+++ src/main/java/com/abhiramdas99/jenkinspipelinejava01/JenkinsPipelineJava01Application.java
@@ -0,0 +1,13 @@
+package com.abhiramdas99.jenkinspipelinejava01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsPipelineJava01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPipelineJava01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/abhiramdas99/jenkinspipelinejava01/JenkinsPipelineJava01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhiramdas99.jenkinspipelinejava01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPipelineJava01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
