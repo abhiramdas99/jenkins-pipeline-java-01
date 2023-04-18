8712047037469d6a6f7174ffc56220ddeefa27ea
abhiramdas99 <abhiramdas99@gmail.com>
2023-04-18 19:10:41 +0530

    welcome page

---
+++ src/main/java/com/abhiramdas99/jenkinspipelinejava01/helloworld/HellowWorldController.java
@@ -0,0 +1,14 @@
+package com.abhiramdas99.jenkinspipelinejava01.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HellowWorldController {
+	
+	@RequestMapping(method=RequestMethod.GET, path="/welcome")
+	public String helloWorld() {
+		return "welcome";
+	}
+}
